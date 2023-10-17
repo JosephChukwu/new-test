33da33b4e11fc6029792934df9198e67dc1a468f
Joseph Chukwu <cjosephc9@gmail.com>
2023-10-17 02:54:27 -0700

    Creatting the Header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 import Home from './pages/Home'
 import About from './pages/About'
 import Profile from './pages/Profile'
-// import Header from './components/Header'
+import Header from './components/Header'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
 export default function App() {
     return <BrowserRouter>
-    {/* <Header/> */}
+    <Header/>
     <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Login' element={<Login/>} />
