7d5127210c1a0836c3ebb0506e3be1b07e98f408
Joseph Chukwu <cjosephc9@gmail.com>
2023-10-17 02:45:34 -0700

    new comit via frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+// import Header from './components/Header'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 export default function App() {
-  return (
-    <div className='text-red-500'>App</div>
-  )
+    return <BrowserRouter>
+    {/* <Header/> */}
+    <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/Register' element={<Register/>} />
+        <Route path='/About' element={<About/>} />
+        <Route path='Profile' element={<Profile/>} />
+    </Routes>
+    </BrowserRouter>
 }
